085a7e6baf782e08a51b0d957e341e7a19afbe64
SP <sp@SPs-Mac-Pro.local>
2014-09-10 20:24:50 -0700

    added OutOfThisWorld project

---
+++ CPOuterSpaceTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  CPOuterSpaceTableViewController.h
+//  Out of this World
+//
+//  Created by SP on 9/6/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CPOuterSpaceTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSMutableArray *planets;
+
+@end
